978c382fce4af342361b083732ede2f69b1b58dc
Mikias Daniel <beldanmik@gmail.com>
2024-08-05 12:04:16 +0300

    added logging service

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,6 +1,12 @@
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.CongifureCors();
+builder.Services.ConfigureIISIntegration(); 
 
 builder.Services.AddControllers();
 
@@ -8,7 +14,19 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else 
+    app.UseHsts();
+
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
