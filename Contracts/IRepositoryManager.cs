8d324a0ef76979bbf397b5cc5f8e620fecfabed9
dot-ruth <ruthgetaneh5@gmail.com>
2024-08-16 11:44:17 +0300

    FEAT|NULL|added a repository manager

--- Contracts/IRepositoryManager.cs
+++ Contracts/IRepositoryManager.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace Contracts
 {
-    public interface ICompanyRepository
+    public interface IRepositoryManager
     {
+        ICompanyRepository Company { get; }
+        IEmployeeRepository Employee { get; }
+        void Save();
     }
 }
